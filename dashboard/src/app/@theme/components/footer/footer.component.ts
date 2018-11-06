7e9b5362d8a957785ebbc1da8cfca6a5ac7b5869
Aniketh01 <anikethgireesh@gmail.com>
2018-11-06 15:29:51 +0530

    Few changes in the footer

--- dashboard/src/app/@theme/components/footer/footer.component.ts
+++ dashboard/src/app/@theme/components/footer/footer.component.ts
@@ -4,12 +4,13 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by">Created with ♥ by <b><a href="https://amfoss.in" target="_blank">amfoss</a></b>
+     inspired by <b><a href="https://akveo.com" target="_blank">akveo</a></b> 2017</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://github.com/amfoss/" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://www.facebook.com/amfoss.in/" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="https://twitter.com/amfoss_in" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://www.instagram.com/amfoss.in/" target="_blank" class="ion ion-social-instagram"></a>
     </div>
   `,
 })
