cf42fcc9f2cf78610a0674b82a7c03d536100211
Aniketh01 <anikethgireesh@gmail.com>
2018-11-17 22:36:12 +0530

    updated html to text converter

--- extractor/htmlconverttext.py
+++ extractor/htmlconverttext.py
@@ -3,17 +3,25 @@
 import sys
 import html2text
 
+html = open(sys.argv[1])
+f = html.read()
+
+w = open("out.txt", "w")
+
 h_parser = html2text.HTML2Text()
 
 h_parser.ignore_links = True
 h_parser.ignore_anchors = True
-h_parser.inline_links = True
+h_parser.inline_links = False
+h_parser.ignore_images = True
+h_parser.ignore_emphasis = True
+h_parser.ignore_links = True
 
+text = h_parser.handle(f)
+text = text.strip(' \t\n\r')
+text = text.replace('\\n', '')
 
-html = open(sys.argv[1])
-f = html.read()
-w = open("out.txt", "w")
-w.write(html2text.html2text(f))
+w.write(text)
 html.close()
 w.close()
 
